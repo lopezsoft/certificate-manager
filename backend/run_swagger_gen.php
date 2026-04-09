f0040803590754bd8403ac64ce3fb5833058b9ad
Lewis Lopez <lopezsoft.com@gmail.com>
2026-04-09 10:21:25 -0500

    chore: remove temporary audit scripts

--- backend/run_swagger_gen.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Run artisan command
-chdir(__DIR__);
-exec('php artisan l5-swagger:generate 2>&1', $output, $code);
-file_put_contents(__DIR__ . '/swagger_gen_result.txt', implode("\n", $output) . "\nEXIT: $code\n");
-
